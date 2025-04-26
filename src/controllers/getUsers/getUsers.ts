c4814998971fe36880c3990877ac861de124ff3d
Vitor Lima Nascimento <vitornascimento923@gmail.com>
2025-04-25 22:37:17 -0300

    feat: add getUserController

---
+++ src/controllers/getUsers/getUsers.ts
@@ -0,0 +1,26 @@
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+    constructor(private readonly getUsersRepository: IGetUsersRepository) {
+
+    }
+
+    async handler() {
+        try {
+            //validar req
+            // direciona chamada para repository
+            const users = await this.getUsersRepository.getUsers()
+
+            return {
+                statusCode: 200,
+                body: users,
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: "Something went wrong.",
+            }
+        }
+    }
+
+}
