18c6d10ba0868b58179767e2a9a88c686362f657
Vitor Lima Nascimento <vitornascimento923@gmail.com>
2025-04-25 22:00:56 -0300

    feat: adc express e servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World")
+import express from 'express';
+import { config } from 'dotenv'
+
+config();
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send("hello world!");
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
