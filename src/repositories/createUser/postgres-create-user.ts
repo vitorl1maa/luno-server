23a7b2e0c24eef1a1fc51139af578da2b9014c06
Vitor Lima Nascimento <vitornascimento923@gmail.com>
2025-05-04 22:35:56 -0300

    feat: add PostgresCreatUserRepository

---
+++ src/repositories/createUser/postgres-create-user.ts
@@ -0,0 +1,27 @@
+import { CreateUserParams, ICreateUserRepository } from "../../controllers/createUser/protocols";
+import { PrismaClient } from "../../generated/prisma";
+import { User } from "../../models/user";
+
+const prisma = new PrismaClient();
+
+export class PostgresCreateUser implements ICreateUserRepository {
+    async createUser(params: CreateUserParams): Promise<User> {
+        try {
+            const user = await prisma.user.create({
+                data: params,
+            });
+
+            return user
+
+        } catch (error: any) {
+            console.error("Erro ao criar usuário:", error);
+
+
+            if (error.code === 'P2002') {
+                throw new Error("E-mail já está em uso.");
+            }
+
+            throw new Error("Erro ao criar usuário no banco de dados.");
+        }
+    }
+}
